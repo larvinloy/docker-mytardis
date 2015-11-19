e07d665422339ea83b95173375dc5a7cc07094a9
Iman Yusuf <iimanyusuf@gmail.com>
2015-11-19 00:34:34 +0000

    docker initialiizing script added; options are mysql, postgres and rmitcas. Default is postgres

--- options/auth/rmitcas/docker_settings.py
+++ options/auth/rmitcas/docker_settings.py
@@ -1,23 +1,3 @@
-import djcelery
-from datetime import timedelta
-import os
-
-DATABASES = {
-    'default': {
-        'ENGINE':'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_ENV_MYSQL_DATABASE', ''),
-        'USER': os.environ.get('DB_ENV_MYSQL_USER', ''),
-        'PASSWORD': os.environ.get('DB_ENV_MYSQL_PASSWORD', ''),
-        'HOST': os.environ.get('DB_PORT_3306_TCP_ADDR', ''),
-        'PORT': os.environ.get('DB_PORT_3306_TCP_PORT', ''),
-        'STORAGE_ENGINE': 'InnoDB',
-        'OPTIONS': {
-            #'init_command': 'SET storage_engine=InnoDB',
-            'charset': 'utf8',
-            'use_unicode': True,
-        },
-    },
-}
 
 # CAS Server default settings
 CAS_ENABLED = True
@@ -28,34 +8,3 @@ CAS_LOGOUT_COMPLETELY = True
 CAS_LOGIN_URL = '/cas/login/'
 
 
-CELERY_RESULT_BACKEND = 'amqp'
-
-STAGING_PATH = "/staging"
-DEFAULT_STORAGE_BASE_DIR = "/store"
-
-
-BROKER_URL ='amqp://{user}:{password}@{hostname}/{vhost}/'.format(
-        user='admin',
-        password=os.environ.get('RABBITMQ_ENV_RABBITMQ_PASS', 'mypass'),
-        hostname='rabbitmq:5672',
-        vhost='')
-
-SYSTEM_LOG_LEVEL = 'DEBUG'
-MODULE_LOG_LEVEL = 'DEBUG'
-
-SYSTEM_LOG_FILENAME = '/logs/request.log'
-MODULE_LOG_FILENAME = '/logs/tardis.log'
-
-CELERYBEAT_SCHEDULE = {
-    "verify-files": {
-        "task": "tardis_portal.verify_dfos",
-        "schedule": timedelta(seconds=30)
-    },
-    # enable this task for the publication workflow
-    # "update-publication-records": {
-    #     "task": "tardis_portal.update_publication_records",
-    #     "schedule": timedelta(seconds=300)
-    # },
-}
-
-djcelery.setup_loader()
