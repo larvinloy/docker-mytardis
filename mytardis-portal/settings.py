3b551e5d11ac827124eb2927e862d1b950632f1a
Ian Thomas <ianedwardthomas@gmail.com>
2016-03-01 16:42:11 +1100

    added fix for 3.7 initial profile bug

--- mytardis-portal/settings.py
+++ mytardis-portal/settings.py
@@ -2,6 +2,9 @@ from tardis.settings_changeme import *
 
 INSTALLED_APPS += ('tardis.apps.mydata',) 
 
+# required workaround for mytardis 3.7 bug
+INSTALLED_APPS = ['tardis.tardis_portal'] + [x for x in INSTALLED_APPS if not x == 'tardis.tardis_portal']
+ 
 import sys
 sys.path.append('/mytardis_settings')
 
